74681515400421994d02000d36e79cda608d1a37
zlodiak <prozaik81-2@yandex.ru>
2017-11-25 17:27:32 +0300

    drag is ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,98 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  title: string = 'My first AGM project';
+  latMap: number = 42.8;
+  lngMap: number = -71.1;
+
+  markerName = '';
+  markerLat = '';
+  markerLng = '';
+  markerDraggable = '';
+
+  markers: marker[] = [
+  	{
+  		name: 'company one',
+  		lat: 42.8,
+  		lng: -71.1,
+  		draggable: true
+  	},
+  	{
+  		name: 'company 2',
+  		lat: 42.1,
+  		lng: -72.1,
+  		draggable: true
+  	},
+  	{
+  		name: 'company 3',
+  		lat: 42.8,
+  		lng: -71.6,
+  		draggable: true
+  	}  	  	
+  ]
+
+  mapClicked($event: any) {
+  	console.log('map clicked');
+
+  	let newMarker = {
+  		name: 'untitled',
+  		lat: $event.coords.lat,
+  		lng: $event.coords.lng,
+  		draggable: false
+  	}
+
+  	this.markers.push(newMarker);
+  };
+
+  clickMarker(marker: marker, index: number) {
+  	console.log('clickMarker', marker.name, index);
+  }
+
+  markerDragEnd(marker: marker, $event: any) {
+  	console.log('markerDragEnd', marker, $event);
+
+  	let updMarker = {
+  		name: marker.name,
+  		lat: marker.lat,
+  		lng: marker.lng,
+  		draggable: false
+  	};
+
+  	let newLat = $event.coords.lat;
+  	let newLng = $event.coords.lng;
+  };
+
+  addMarker() {
+  	console.log('addMarker');
+
+  	let isDraggable;
+
+  	if(this.markerDraggable == 'yes') {
+  		isDraggable = true;
+  	} else {
+  		isDraggable = false;
+  	}
+
+  	let newMarker = {
+  		name: this.markerName,
+  		lat: +this.markerLat,
+  		lng: +this.markerLng,
+  		draggable: isDraggable
+  	};
+
+  	this.markers.push(newMarker);
+
+  	console.log(this.markers);
+  };
+
+}
+
+
+interface marker {
+	name?: string;
+	lat: number;
+	lng: number;
+	draggable: boolean;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +12,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+		AgmCoreModule.forRoot({
+		  apiKey: 'AIzaSyDT2NO8RgOBPpi3Hph-sjfyE1zyRPAoMnQ'
+		})    
   ],
   providers: [],
   bootstrap: [AppComponent]
